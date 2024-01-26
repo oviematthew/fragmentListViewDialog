070e8dc95c6cabb688525a165fa18ea03c363fdd
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-01-25 20:58:26 -0500

    completed dialog

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.lab1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.DialogFragment
 import com.example.lab1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SurveyFragment.SendMessages {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -20,6 +21,13 @@ class MainActivity : AppCompatActivity() {
         binding.instructionsBtn.setOnClickListener{
             switchActivity()
         }
+
+        //popup dialog on click of survey button
+        binding.surveyBtn.setOnClickListener{
+            val surveyFrag: DialogFragment? = SurveyFragment
+                .newInstance(R.string.confirm)
+            surveyFrag!!.show(supportFragmentManager, "dialog")
+        }
     }
 
     //Switch screen
@@ -29,5 +37,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+    override fun choiceMade(msg: Int?)
+    {
+        binding.surveyTextView.text = "My choice is: ${SurveyFragment.levels[msg!!]}"
+        SurveyFragment.checkedItem = msg
+    }
 }
---
+++ app/src/main/java/com/example/lab1/SurveyFragment.kt
@@ -0,0 +1,73 @@
+package com.example.lab1
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class SurveyFragment : DialogFragment() {
+
+    private var sendMessages: SendMessages? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_survey, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(activity)
+            .setTitle(R.string.choice)
+            .setSingleChoiceItems(levels, checkedItem, DialogInterface.OnClickListener {
+                    dialog, which ->
+                sendMessages!!.choiceMade(which)
+            })
+            .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener {
+                    dialog, which -> Log.d("confirmation dialog", "positive button")
+            })
+            .setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener {
+                    dialog, which -> Log.d("confirmation dialog", "negative button")
+            }).create()
+
+    }
+
+    companion object {
+
+        var levels = arrayOf( "Level 1", "Level 2", "Level 3", "Level 4")
+        var checkedItem = 1
+
+        @JvmStatic
+        open fun newInstance(title: Int):
+                SurveyFragment? {
+            val fragment = SurveyFragment()
+            val args = Bundle()
+            args.putInt("title", title)
+            fragment.arguments =args
+            return fragment
+        }
+    }
+
+    // attach the interface
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // send information through interface
+        sendMessages = context as SendMessages
+    }
+
+    // Interface to send information from the fragment to MainActivity
+    internal interface SendMessages {
+        fun choiceMade(msg:Int?)
+    }
+}
